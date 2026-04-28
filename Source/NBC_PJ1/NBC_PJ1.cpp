31c99bb41a38bd359bc6babf9c0f16cca39583bf
sydxis <k01023100681@gmail.com>
2026-04-28 22:22:20 +0900

    Initial commit

---
+++ Source/NBC_PJ1/NBC_PJ1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NBC_PJ1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NBC_PJ1, "NBC_PJ1" );
